fc2fb7a0d208becea2a3ffe6e7d59862d3a3793f
Joe Puzzo <jpuzzo@tesla.com>
2022-09-20 09:44:39 -0700

    added servo

---
+++ servo.js
@@ -0,0 +1,21 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+
+  const servo = new five.Servo({
+    pin: 13,
+    startAt: 15
+  });
+
+  this.repl.inject({
+    to: (loc, speed = 500) =>{
+      servo.to(loc,speed);
+    },
+    center: () =>{
+      servo.center();
+    },
+  });
+
+});
+
